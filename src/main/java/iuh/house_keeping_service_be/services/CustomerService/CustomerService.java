6214fd5c02ec46f03c4f136e6be054e277337eed
HKThanh <21086351.thanh@student.iuh.edu.vn>
2025-08-22 10:52:26 +0700

    UPDATE: Employee service

--- src/main/java/iuh/house_keeping_service_be/services/CustomerService/CustomerService.java
+++ src/main/java/iuh/house_keeping_service_be/services/CustomerService/CustomerService.java
@@ -1,6 +1,5 @@
 package iuh.house_keeping_service_be.services.CustomerService;
 
-import iuh.house_keeping_service_be.enums.Role;
 import iuh.house_keeping_service_be.models.Customer;
 
 public interface CustomerService {
--- src/main/java/iuh/house_keeping_service_be/services/EmployeeService/EmployeeService.java
+++ src/main/java/iuh/house_keeping_service_be/services/EmployeeService/EmployeeService.java
@@ -1,6 +1,5 @@
 package iuh.house_keeping_service_be.services.EmployeeService;
 
-import iuh.house_keeping_service_be.enums.Role;
 import iuh.house_keeping_service_be.models.Employee;
 
 public interface EmployeeService {
