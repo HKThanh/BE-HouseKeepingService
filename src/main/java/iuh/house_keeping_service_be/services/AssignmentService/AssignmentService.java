f65f9542f173a8a16b22bc5d1cd5599fc600661e
HKThanh <21086351.thanh@student.iuh.edu.vn>
2025-09-30 17:57:28 +0700

    FEAT: add check-in and check-out functionality for assignments with corresponding test cases

--- src/main/java/iuh/house_keeping_service_be/services/AssignmentService/AssignmentService.java
+++ src/main/java/iuh/house_keeping_service_be/services/AssignmentService/AssignmentService.java
@@ -1,5 +1,6 @@
 package iuh.house_keeping_service_be.services.AssignmentService;
 
+import iuh.house_keeping_service_be.dtos.Assignment.request.AssignmentActionRequest;
 import iuh.house_keeping_service_be.dtos.Assignment.request.AssignmentCancelRequest;
 import iuh.house_keeping_service_be.dtos.Assignment.response.AssignmentDetailResponse;
 import iuh.house_keeping_service_be.dtos.Assignment.response.BookingSummary;
@@ -13,4 +14,7 @@ public interface AssignmentService {
 
     List<BookingSummary> getAvailableBookings(String employeeId, int page, int size);
     AssignmentDetailResponse acceptBookingDetail(String detailId, String employeeId);
+
+    AssignmentDetailResponse checkIn(String assignmentId, AssignmentActionRequest request);
+    AssignmentDetailResponse checkOut(String assignmentId, AssignmentActionRequest request);
 }
